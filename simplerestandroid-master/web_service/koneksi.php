d039728ed3b03336944af4bbac2fa6f7fed3c8fc
Alterga <55912766+Alterga@users.noreply.github.com>
2020-04-21 03:51:47 +0700

    Add files via upload

---
+++ simplerestandroid-master/web_service/koneksi.php
@@ -0,0 +1,7 @@
+<?php
+ define('HOST','localhost');
+ define('USER','id12777864_mhs');
+ define('PASS','12345678');
+ define('DB','id12777864_mahasiswa');
+
+ $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
